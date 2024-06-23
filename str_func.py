bbc72b5a8b2f6c4155ccbd51ae201dd7431b33d8
KarelinIvan <ivan.karelin.1993@mail.ru>
2024-06-23 15:43:00 +0300

    Второй коммит

--- str_func.py
+++ str_func.py
@@ -1,7 +1,24 @@
 def uppercase_input():
+    """
+    принимает на вход строку и возвращает ее со всеми заглавными буквами
+    :return:
+    """
     user_input = input("Введите текст: ")
     return user_input.upper()
 
 
 uppercase_string = uppercase_input()
 print(uppercase_string)
+
+
+def capitalize_words_input():
+    """
+    делает заглавными первые буквы каждого слова в строке, поступившей на вход функции
+    :return:
+    """
+    user_input = input("Введите строку: ")
+    return user_input.title()
+
+
+capitalize_string = capitalize_words_input()
+print(capitalize_string)
